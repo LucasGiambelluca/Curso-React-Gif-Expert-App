2821c36f416940add5600f460df57e7de3f5a6b9
fxstmp <lucasdavigiambelluca@gmail.com>
2022-05-09 04:07:18 -0300

    changes to https

--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -1,5 +1,5 @@
 export const getGif = async (category)=>{
-    const url = `http://api.giphy.com/v1/gifs/search?key=FDLzeCzgpnxOmmS3l2X37X1JCSeXJY0L&q=${encodeURI(category)}&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?key=FDLzeCzgpnxOmmS3l2X37X1JCSeXJY0L&q=${encodeURI(category)}&limit=10`
     const response = await fetch(url)
 
     const {data} = await response.json();
